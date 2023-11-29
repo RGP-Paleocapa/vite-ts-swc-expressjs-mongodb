38b5e7c47493d17120cda41351681f14df4a7ecc
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-11-29 15:41:31 +0100

    Integrated mongoose

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,47 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
+// Define a type for your item
+type Item = {
+  _id: string;
+  name: string;
+  description: string;
+};
+
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [items, setItems] = useState<Item[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/greeting')
+    fetch('http://localhost:3000/api/items')
       .then(response => response.json())
-      .then(data => setGreeting(data.message))
-      .catch(error => console.error('Error fetching data:', error));
+      .then(data => setItems(data as Item[]))
+      .catch(error => console.error('Error fetching items:', error));
   }, []);
+
+  // Function to add a new item
+  const addItem = async () => {
+    const newItem = { name: "New Item", description: "Description of new item" };
+    await fetch('http://localhost:3000/api/items', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newItem)
+    });
+    // Reload items after adding
+    fetch('http://localhost:3000/api/items')
+      .then(response => response.json())
+      .then(data => setItems(data as Item[]))
+      .catch(error => console.error('Error fetching items:', error));
+  };
 
   return (
     <>
       <h1 className='text-7xl'>Hello, World</h1>
-      <p>{greeting}</p>
+      <button onClick={addItem}>Add Item</button>
+      <ul>
+        {items.map(item => (
+          <li key={item._id}>{item.name}: {item.description}</li>
+        ))}
+      </ul>
     </>
   );
 }
